7b7678df886145cc835b6b0fd7205f8e8c727377
Rikard Glans <rikard@ecx.se>
2016-08-10 23:58:04 +0200

    Let's do it this way instead.

--- LegionInvasionTimer.lua
+++ LegionInvasionTimer.lua
@@ -1,4 +1,3 @@
-
 local name = ...
 local candy = LibStub("LibCandyBar-3.0")
 local media = LibStub("LibSharedMedia-3.0")
@@ -21,49 +20,60 @@ local header = frame:CreateFontString("TargetPercentText", "OVERLAY", "TextStatu
 header:SetAllPoints(frame)
 header:SetText(name)
 
-local function startBar(timeLeft, done)
-	local bar = candy:New(media:Fetch("statusbar", "BantoBar"), 180, 15)
-	local label = "Invasion"
-	if done then
-		label = label .. " (done)"
-	end
-	bar:SetLabel(label)
-	bar.candyBarLabel:SetJustifyH("LEFT")
-	bar:SetDuration(timeLeft)
-	bar:SetIcon(236292) -- Interface\\Icons\\Ability_Warlock_DemonicEmpowerment
-	bar:SetPoint("TOP", LegionInvasionTimer, "BOTTOM")
-	bar:Start()
+local function allAreDone()
+  local done = false
+
+  for i = 1, 300 do
+    local name, timeLeftMinutes, rewardQuestID = GetInvasionInfo(i)
+    if timeLeftMinutes and timeLeftMinutes > 0 then
+      done = IsQuestFlaggedCompleted(rewardQuestID)
+    end
+  end
+
+  return done
+end
+
+local function startBar(timeLeft)
+  local bar = candy:New(media:Fetch("statusbar", "BantoBar"), 180, 15)
+  local label = "Invasion"
+  if allAreDone() then
+    label = label .. " (done)"
+  end
+  bar:SetLabel(label)
+  bar.candyBarLabel:SetJustifyH("LEFT")
+  bar:SetDuration(timeLeft)
+  bar:SetIcon(236292) -- Interface\\Icons\\Ability_Warlock_DemonicEmpowerment
+  bar:SetPoint("TOP", LegionInvasionTimer, "BOTTOM")
+  bar:Start()
 end
 
 local function runOnLogin()
-	local found = false
+  local found = false
 
-	for i = 1, 300 do
-		local name, timeLeftMinutes, rewardQuestID = GetInvasionInfo(i)
-		if timeLeftMinutes and timeLeftMinutes > 0 then
-			found = true
-			local isDone = IsQuestFlaggedCompleted(rewardQuestID)
-			legionInvasionTimerDB = {GetTime(), timeLeftMinutes, isDone}
+  for i = 1, 300 do
+    local name, timeLeftMinutes, rewardQuestID = GetInvasionInfo(i)
+    if timeLeftMinutes and timeLeftMinutes > 0 then
+      found = true
+      legionInvasionTimerDB = {GetTime(), timeLeftMinutes}
 
-			startBar(timeLeftMinutes * 60, isDone)
-			break
-		end
-	end
+      startBar(timeLeftMinutes * 60)
+      break
+    end
+  end
 
-	if not found and legionInvasionTimerDB then
-		local t, rem, done = legionInvasionTimerDB[1], legionInvasionTimerDB[2], legionInvasionTimerDB[3]
-		if t and rem then
-			found = true
-			local deduct = (GetTime() - t) / 60
-			local timeLeftMinutes = rem - deduct
-			startBar(timeLeftMinutes * 60, done)
-		end
-	end
+  if not found and legionInvasionTimerDB then
+    local t, rem = legionInvasionTimerDB[1], legionInvasionTimerDB[2]
+    if t and rem then
+      found = true
+      local deduct = (GetTime() - t) / 60
+      local timeLeftMinutes = rem - deduct
+      startBar(timeLeftMinutes * 60)
+    end
+  end
 
-	if not found then
-		C_Timer.After(7, runOnLogin) -- The very first login doesn't have GetInvasionInfoByMapAreaID data fast enough, delay it
-	end
+  if not found then
+    C_Timer.After(7, runOnLogin) -- The very first login doesn't have GetInvasionInfoByMapAreaID data fast enough, delay it
+  end
 end
 
 frame:SetScript("OnEvent", runOnLogin)
-
